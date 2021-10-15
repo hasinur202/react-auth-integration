5a2de4a621b68d1eeaf6db70fa9604c32593d01b
Hasinur Rahman <hasinur202@gmail.com>
2021-10-16 01:14:56 +0600

    initial commit

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,11 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDPyR6aDemwU7-vwiQ-8luUpVVJjUhy0JE",
+    authDomain: "simple-firebase-auth-c65f3.firebaseapp.com",
+    projectId: "simple-firebase-auth-c65f3",
+    storageBucket: "simple-firebase-auth-c65f3.appspot.com",
+    messagingSenderId: "575979195262",
+    appId: "1:575979195262:web:7a4f1a00ee3f15a15ec12f"
+  };
+
+  export default firebaseConfig
